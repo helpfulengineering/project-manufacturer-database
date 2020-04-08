856ac36a04d85c79384f0431e37bce8eb664be1c
RM <RRMoelker@users.noreply.github.com>
2020-04-08 14:16:23 +0200

    Parallel mutation calls

--- import/uplink/client.js
+++ import/uplink/client.js
@@ -1,5 +1,6 @@
 import log from "loglevel";
 import aclient from "apollo-client";
+import PromisePool from 'es6-promise-pool';
 
 const {ApolloClient} = aclient;
 import inmemory from 'apollo-cache-inmemory';
@@ -11,6 +12,8 @@ const {createHttpLink} = alink;
 import gql from 'graphql-tag';
 import fetch from 'node-fetch'
 import {InsertQuery} from "./queries.js";
+
+const concurrency = 3; // The number of concurrent uploads
 
 export const createClient = (uri) => {
   const client = new ApolloClient({
@@ -24,45 +27,64 @@ export const createClient = (uri) => {
   return client;
 };
 
+const mutateSingleEntity = (client, entity) => {
+  const { name } = entity;
+  const site = entity.sites[0]; // TODO loop
+  const equipment = site.equipments[0]; // TODO loop
+  const contact = entity.contacts[0];
+  return client.mutate({
+    mutation: gql`${InsertQuery}`,
+    variables: {
+      name: entity.name,
+      certifications: '',
+      experience: entity.experience,
+      notes: entity.notes,
+      scale: entity.scale,
+      email: contact.email,
+      country: site.country,
+      city: site.city,
+      lat: site.lat,
+      lng: site.lng,
+      location: {
+        type: "Point",
+        coordinates: [site.lng, site.lat]
+      },
+      model: equipment.model,
+      brand: "",
+      quantity: equipment.quantity
+    }
+  });
+};
+
 export const uploadData = async (client, entities) => {
   const total = entities.length;
   log.info(`uploading ${total} entities to DB`);
   let errorCount = 0;
 
-  for (const [i, entity] of entities.entries()) {
-    log.debug(`uploading: ${i}/${total}`);
-    const site = entity.sites[0]; // TODO loop
-    const equipment = site.equipments[0]; // TODO loop
-    const contact = entity.contacts[0];
-    await client.mutate({
-      mutation: gql`${InsertQuery}`,
-      variables: {
-        name: entity.name,
-        certifications: '',
-        experience: entity.experience,
-        notes: entity.notes,
-        scale: entity.scale,
-        email: contact.email,
-        country: site.country,
-        city: site.city,
-        lat: site.lat,
-        lng: site.lng,
-        location: {
-          type: "Point",
-          coordinates: [site.lng, site.lat]
-        },
-        model: equipment.model,
-        brand: "",
-        quantity: equipment.quantity
-      }
-    })
-    .then(result => log.debug(`upload success ${i}`))
-    .catch(error => {
-      log.error(`promise catch: problem uploading ${i}, ${entity.name}: ${error}`);
-      errorCount += 1;
-      log.error('moving to next item');
-    });
+  const promiseGenerator = function * () {
+    for(const [i, entity] of entities.entries()) {
+      log.debug(`uploading: ${i}/${total}`);
+      const uploadPromise = mutateSingleEntity(client, entity)
+        .then(result => log.debug(`${i} upload success`))
+        .catch(error => {
+          log.error(`${i} promise catch: problem uploading, ${entity.name}: ${error}`);
+          log.error(`${i} moving to next item`);
+          errorCount += 1;
+        });
+      yield uploadPromise;
+    }
+  };
+
+  const promiseIterator = promiseGenerator();
+  const pool = new PromisePool(promiseIterator, concurrency);
+  const poolPromise = pool.start();
+
+  try {
+    await poolPromise;
+  } catch(error) {
+    log.error('Some promise in pool rejected: ' + error.message)
   }
+
   if (errorCount > 0) {
     log.error(`total of ${errorCount} out of ${total} items failed to upload`);
   } else {
