df773f73168ecc194c9f2a9fb368a1769eb199ad
RM <RRMoelker@users.noreply.github.com>
2020-04-22 18:50:40 +0200

    Test working without token

--- frontend/src/auth/util.js
+++ frontend/src/auth/util.js
@@ -2,6 +2,10 @@ import jwt_decode from "jwt-decode";
 import get from 'lodash/get';
 
 export const getUserId = (token) => {
-  const decoded = jwt_decode(token);
-  return get(decoded, ['https://hasura.io/jwt/claims', 'x-hasura-user-id']);
+  if (token) {
+    const decoded = jwt_decode(token);
+    return get(decoded, ['https://hasura.io/jwt/claims', 'x-hasura-user-id']);
+  } else {
+    return undefined;
+  }
 };
